67ac320a44a036449ba090bbc45f32e17bd4e6ce
A17B <61550989+A17B@users.noreply.github.com>
2025-09-02 12:10:29 +0530

    added workflows

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+const express = require("express");
+
 console.log("Hello, this is a simple Node.js project!");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express 🚀");
+});
+
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
